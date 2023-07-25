da6c322afeeb3688fada595d88fe27b24a247074
Piyush Singh <90254632+Piyushsingh03@users.noreply.github.com>
2023-07-25 13:21:36 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const bucket = "my-airbnb-booking-app";
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/uploads", express.static(__dirname + "/api/uploads"));
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 app.use(
   cors({
